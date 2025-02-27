d3517fdb2a4db8ee90fd9b032c0a207b7151f85b
TheJabbs <92254250+TheJabbs@users.noreply.github.com>
2025-02-27 20:49:01 +0200

    validation pipe

--- floucca_back/src/main.ts
+++ floucca_back/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TypeTransformPipe } from './pipes/TypeTransformPipe';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   app.useGlobalPipes(new TypeTransformPipe());
+  app.useGlobalPipes(new ValidationPipe());
+
 
 
 

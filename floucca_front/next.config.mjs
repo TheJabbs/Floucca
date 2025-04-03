2493d3bc9a84f00660a0a852c90effaf089027e3
mayaamasri <maya.h.masri@std.balamand.edu.lb>
2025-04-03 21:25:58 +0300

    form redirection

--- floucca_front/next.config.mjs
+++ floucca_front/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-};
-
-export default nextConfig;
+    async redirects() {
+      return [
+        {
+          source: '/do', // The URL to redirect from
+          destination: '/do/forms/effort&landing', // The URL to redirect to
+          permanent: true, // Set to `false` for a temporary redirect (302)
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  

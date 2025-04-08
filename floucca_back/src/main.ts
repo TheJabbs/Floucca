1e19037d2a02ca4f6124258dc6f8888844d7a8cd
chabelfakhry <charbel.b.fakhry@gmail.com>
2025-04-08 13:47:38 +0300

    fix cors

--- floucca_back/src/main.ts
+++ floucca_back/src/main.ts
@@ -6,12 +6,19 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.enableCors();
+    app.enableCors({
+        origin: ['http://13.37.108.197:3000', 'http://localhost:3000'], // List multiple allowed origins
+        methods: 'GET,POST,PUT,DELETE',       // Specify allowed HTTP methods
+        allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
+        credentials: true,  // Allow cookies if necessary
+    });
+
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
         }),
-        new TypeTransformPipe());
+        new TypeTransformPipe(),
+    );
 
 
     await app.listen(4000);

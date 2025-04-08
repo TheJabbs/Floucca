4de9451b523396292ffe5e0ae37324d04598c95f
chabelfakhry <charbel.b.fakhry@gmail.com>
2025-04-08 13:58:16 +0300

    f

--- floucca_back/src/main.ts
+++ floucca_back/src/main.ts
@@ -7,11 +7,12 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ['http://13.37.108.197:3000', 'http://localhost:3000'], // List multiple allowed origins
-        methods: 'GET,POST,PUT,DELETE',       // Specify allowed HTTP methods
-        allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
-        credentials: true,  // Allow cookies if necessary
+        origin: ['http://13.37.108.197:3000', 'http://localhost:3000'],
+        methods: 'GET,POST,PUT,DELETE,OPTIONS',
+        allowedHeaders: 'Content-Type, Authorization',
+        credentials: true,
     });
+    
 
     app.useGlobalPipes(
         new ValidationPipe({

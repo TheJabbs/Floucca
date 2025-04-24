eb5d0457ef1bcc4e56ba08f3e0c97eee83243aea
RebeccaSle <rebeccasleiman03@gmail.com>
2025-04-24 17:02:27 +0300

    coop

--- floucca_back/src/backend/coop/coop.controller.ts
+++ floucca_back/src/backend/coop/coop.controller.ts
@@ -1,52 +1,49 @@
-import {Body, Controller, Delete, Get, Param, Post, Put} from "@nestjs/common";
-import {CoopService} from "./coop.service";
-import {CoopDto} from "./dto/coop.dto";
-import {mapCoopToReturnCoopInterface} from "./mapper/coop.mapper";
-import {idDTO} from "../../shared/dto/id.dto";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { CoopService } from "./coop.service";
+import { CoopDto } from "./dto/coop.dto";
+import { mapCoopToReturnCoopInterface } from "./mapper/coop.mapper";
 
 @Controller('api/coop')
 export class CoopController {
-    constructor(private coopService: CoopService) {
+  constructor(private coopService: CoopService) {}
+
+  @Get('/all')
+  getAllCoops() {
+    return this.coopService.getAllCoops();
+  }
+
+  @Get('/:id')
+  getCoopById(@Param('id') id: number) {
+    return this.coopService.getCoopById(id);
+  }
+
+  @Post('/create')
+  createCoop(@Body() coop: CoopDto) {
+    const check = this.coopService.getCoopById(coop.coop_code);
+    if (check) {
+      return "Coop already exists";
     }
 
-    @Get('/all')
-    getAllCoops() {
-        return this.coopService.getAllCoops();
+    return this.coopService.createCoop(mapCoopToReturnCoopInterface(coop));
+  }
+
+  @Put('/update/:id')
+  updateCoop(@Body() coop: CoopDto, @Param('id') id: number) {
+    const check = this.coopService.getCoopById(id);
+    if (!check) {
+      return "Coop does not exist";
     }
 
-    @Get('/:id')
-    getCoopById(@Param('id') id: idDTO) {
-        return this.coopService.getCoopById(id.id);
+    return this.coopService.updateCoop(id, mapCoopToReturnCoopInterface(coop));
+  }
+
+  @Delete('/delete/:id')
+  deleteCoop(@Param('id') id: number) {
+    const check = this.coopService.getCoopById(id);
+    if (!check) {
+      return "Coop does not exist";
     }
 
-    @Post('/create')
-    createCoop(@Body () coop : CoopDto){
-        const check = this.coopService.getCoopById(coop.coop_code);
-        if (check){
-            return "Coop already exists"
-        }
-
-        return this.coopService.createCoop(mapCoopToReturnCoopInterface(coop));
-
-    }
-
-    @Put('/update/:id')
-    updateCoop(@Body() coop : CoopDto, @Param('id') id: idDTO){
-        const check = this.coopService.getCoopById(id.id);
-        if (!check){
-            return "Coop does not exist"
-        }
-        return this.coopService.updateCoop(id.id,mapCoopToReturnCoopInterface(coop));
-    }
-
-    @Delete('/delete/:id')
-    deleteCoop(@Param('id') id: idDTO){
-        const check = this.coopService.getCoopById(id.id);
-        if (!check){
-            return "Coop does not exist"
-        }
-        return this.coopService.deleteCoop(id.id);
-    }
-
-
+    return this.coopService.deleteCoop(id);
+  }
 }

5dc1df34d930a282e8af80bdece7e7afe458432a
mayaamasri <maya.h.masri@std.balamand.edu.lb>
2025-04-27 20:18:15 +0300

    router names adjustments

--- floucca_front/app/dashboard-admin/page.tsx
+++ floucca_front/app/dashboard-admin/page.tsx
@@ -75,7 +75,7 @@ const AdminDashboard = () => {
               </button>
               <button
                 className="w-full flex items-center p-2 rounded-md hover:bg-blue-50 text-gray-700"
-                onClick={() => router.push("/dashboard")}
+                onClick={() => router.push("/dashboard-admin/reports")}
               >
                 <FileChartColumnIncreasing className="w-4 h-4 mr-3 text-blue-600" />
                 <span>Reports and Stats</span>

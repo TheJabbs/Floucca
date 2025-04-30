161ff89c124bedf0299905fb5ccf40d1de212fd2
TheJabbs <92254250+TheJabbs@users.noreply.github.com>
2025-04-30 12:53:56 +0300

    We getting somewhere

--- floucca_back/src/backend/fleet_senses/interface/sense_form_content.interface.ts
+++ floucca_back/src/backend/fleet_senses/interface/sense_form_content.interface.ts
@@ -1,6 +1,7 @@
-import {CreateFormDto} from "../../form/dto";
-import {CreateBoatDetailsDto} from "../../boat_details/dto";
-import {CreateGearUsageDto} from "../../gear_usage/dto";
+import {CreateFormDto} from "../../form/dto/create_form.dto";
+import {CreateBoatDetailsDto} from "../../boat_details/dto/create_boatDetails.dto";
+import {CreateGearUsageDto} from "../../gear_usage/dto/create_gear_usage.dto";
+
 
 export interface SenseFormContentInterface{
     form: CreateFormDto;

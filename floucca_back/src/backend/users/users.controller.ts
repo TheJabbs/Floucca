d1a6f414779f8264f722207dcc19e7147c1100fd
RebeccaSle <rebeccasleiman03@gmail.com>
2025-05-10 22:39:23 +0300

    lol

--- floucca_back/src/backend/users/users.controller.ts
+++ floucca_back/src/backend/users/users.controller.ts
@@ -4,7 +4,7 @@ import { CreateUserWithDetailsDto } from "./dto/createUserWithDetails.dto";
 import { UpdateUserWithDetailsDto } from "./dto/updateUserWithDetails.dto";
 import { User } from "./interfaces/users.interface";
 import { ResponseMessage } from "src/shared/interface/response.interface";
-//auth
+//auth and role restriction enumerator 
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { UseGuards } from '@nestjs/common';
@@ -15,7 +15,10 @@ import { RoleEnum } from 'src/auth/enums/role.enum';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('admin-create')
+  //might exclude admin if not his role 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(RoleEnum.ADMIN, RoleEnum.SUPER_ADMIN)  
+@Post('admin-create')
 async adminCreatesUser(
   @Body() body: CreateUserWithDetailsDto
 ): Promise<ResponseMessage<{ user_id: number }>> {
@@ -35,7 +38,8 @@ async adminCreatesUser(
     return this.userService.findAllUsers();
   }
 
-  //@UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(RoleEnum.ADMIN, RoleEnum.SUPER_ADMIN)    
   @Get(":user_id")
   async findUserById(@Param("user_id") user_id: string, @Req() req): Promise<User | null> {
     const id = parseInt(user_id, 10);
